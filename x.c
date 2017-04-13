2f657755707183d6dedd7041088323a525c21010
evatux <evatux@gmail.com>
2017-04-13 20:54:38 +0000

    bar should return 2

--- x.c
+++ x.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <assert.h>
 
-int bar() { return 1; }
+int bar() { return 2; }
 int foo() { return 1; }
 
 int main(int argc, char **argv) {
     assert(foo() == 1);
-    assert(bar() == 1);
+    assert(bar() == 2);
     printf("hello %s\n", argc > 1 ? argv[1] : "world");
     return 0;
 }
