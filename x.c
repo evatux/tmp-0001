d52ab20d4d3fc641501692a62d7080b2e157c26e
evatux <evatux@gmail.com>
2017-04-13 16:02:51 +0000

    add test for foo()

--- x.c
+++ x.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include <assert.h>
 
 int foo() { return 1; }
 
 int main(int argc, char **argv) {
+    assert(foo() == 1);
     printf("hello %s\n", argc > 1 ? argv[1] : "world");
     return 0;
 }
